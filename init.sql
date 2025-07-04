cdfb818dc86f722406887289af5d52c767bf9800
Juan P Murdolo <jpmurdolo@gmail.com>
2025-07-04 17:52:57 -0300

    Tested

--- init.sql
+++ init.sql
@@ -1,23 +1,19 @@
 CREATE TABLE IF NOT EXISTS users (
   id SERIAL PRIMARY KEY,
   telegram_id TEXT UNIQUE NOT NULL
-  username TEXT UNIQUE NOT NULL,
-  email TEXT,
-  full_name TEXT,
 );
 
 CREATE TABLE IF NOT EXISTS expenses (
   id SERIAL PRIMARY KEY,
   user_id INTEGER NOT NULL REFERENCES users(id),
   description TEXT NOT NULL,
-  telegram_id TEXT NOT NULL,
   amount MONEY NOT NULL,
   category TEXT NOT NULL,
   added_at TIMESTAMP NOT NULL
 );
 
 -- Add the users you want to allow to use the bot
-INSERT INTO users (telegram_id, username, email, full_name)
+INSERT INTO users (telegram_id)
 VALUES 
-('123456789', 'example_user', 'test@test.com', 'Example User'),
+('123456789'),
 ON CONFLICT (telegram_id) DO NOTHING;
