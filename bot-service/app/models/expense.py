6a724f74104c426f489b864eebdcd5a90482df84
Juan P Murdolo <jpmurdolo@gmail.com>
2025-07-04 13:36:56 -0300

    Change in database

--- bot-service/app/models/expense.py
+++ bot-service/app/models/expense.py
@@ -1,8 +1,6 @@
-from sqlalchemy import Column, Integer, Text, ForeignKey, TIMESTAMP
+from sqlalchemy import Column, Integer, Text, ForeignKey, TIMESTAMP, Numeric
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import declarative_base
-from sqlalchemy_utils import MoneyType
-from sqlalchemy_utils.types import Money
 from app.models.base import Base
 
 
@@ -11,6 +9,6 @@ class Expense(Base):
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     description = Column(Text, nullable=False)
-    amount = Column(Money, nullable=False)
+    amount = Column(Numeric(10,2), nullable=False)
     category = Column(Text, nullable=False)
     added_at = Column(TIMESTAMP, nullable=False)
