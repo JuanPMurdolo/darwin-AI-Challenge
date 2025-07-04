7178c98d86ebe0ee0380f8a1e3a64c39983a1e7f
Juan P Murdolo <jpmurdolo@gmail.com>
2025-07-04 17:52:57 -0300

    Tested

--- bot-service/app/models/user.py
+++ bot-service/app/models/user.py
@@ -7,7 +7,4 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
-    username = Column(String, unique=True, nullable=False)  # ✅ ESTA
     telegram_id = Column(String, unique=True, nullable=True)
-    email = Column(String)
-    full_name = Column(String)
