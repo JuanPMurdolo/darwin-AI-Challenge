d0cb242cf7845fd11c48c4591fc5a9125914089f
Juan P Murdolo <jpmurdolo@gmail.com>
2025-07-04 23:57:08 -0300

    Changing to groq

--- bot-service/app/handlers/langchain_handler.py
+++ bot-service/app/handlers/langchain_handler.py
@@ -1,15 +1,15 @@
-from langchain_openai import ChatOpenAI
+from groq import Groq
 from langchain_core.messages import HumanMessage
 from app.core.config import OPENAI_API_KEY
 import logging
 import json
 import os
 
-llm = ChatOpenAI(
-    api_key=os.getenv("GROQ_API_KEY"),  
-    base_url="https://api.groq.com/openai/v1",  
-    model=os.getenv("MODEL_NAME"),  # Replace with your Groq model name
+client = Groq(
+    # This is the default and can be omitted
+    api_key=os.environ.get("GROQ_API_KEY"),
 )
+
 logger = logging.getLogger(__name__)
 
 CATEGORIES = [
@@ -25,11 +25,20 @@ You are an expense analyzer. The user wrote: "{text}".
 Extract the category (e.g. Food, Transport) from this categories "{CATEGORIES}", amount (as float), and a short description. Don't use any other words or context that the user might provide. Just expenses.
 Respond in JSON format like this: {{"category": "Food", "amount": 10.5, "description": "Pizza"}}
 """
-    try:
-        response = await llm.ainvoke(prompt)
-        logger.info(f"LangChain response: {response.content}")
 
-        parsed = json.loads(response.content)
+    try:
+        chat_completion = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": "{prompt}",
+                }
+            ],
+            model=os.environ.get("MODEL_NAME"),
+        )
+        logger.info(f"LangChain response: {chat_completion.choices[0].message.content}")
+
+        parsed = json.loads(chat_completion.choices[0].message.content)
         return parsed["category"], float(parsed["amount"]), parsed["description"]
 
     except Exception as e:
