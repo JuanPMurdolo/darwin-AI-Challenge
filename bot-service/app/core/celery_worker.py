b39f12403ac69b2bce052266265dc949540debb8
Juan P Murdolo <jpmurdolo@gmail.com>
2025-07-24 16:16:27 -0300

    added Celery os env

--- bot-service/app/core/celery_worker.py
+++ bot-service/app/core/celery_worker.py
@@ -1,9 +1,11 @@
 from celery import Celery
 import os
 
+CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL")
+CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND")
+
 celery_app = Celery(
-    "analytics_tasks",
-    broker=os.getenv("CELERY_BROKER_URL", "redis://localhost:6379/0"),
-    backend=os.getenv("CELERY_RESULT_BACKEND", "redis://localhost:6379/0"),
-    include=["app.tasks.analytics"]
+    __name__,
+    broker=CELERY_BROKER_URL,
+    backend=CELERY_RESULT_BACKEND
 )
