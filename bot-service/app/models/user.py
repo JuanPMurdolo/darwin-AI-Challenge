375733b70fdf4145b43f7b8a09f19e792b0ddcf1
Juan P Murdolo <jpmurdolo@gmail.com>
2025-07-24 15:25:34 -0300

    Changed id to String

--- bot-service/app/models/user.py
+++ bot-service/app/models/user.py
@@ -1,10 +1,10 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, BigInteger
 from app.models.base import Base
 
 class User(Base):
     
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True)
+    id = Column(BigInteger, primary_key=True)
     telegram_id = Column(String, unique=True, nullable=True)
